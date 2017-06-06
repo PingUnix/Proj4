ed955efd97296e35003b2cdbe6bc0f7cfc1ee0f9
SandyLinux <mailliuping@gmail.com>
2017-06-06 14:59:11 -0400

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
+   HEROES: Hero[] = [
+    { id: 11, name:'mr nice'},
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+
+  ];
+  heroes =  this.HEROES;
   title = 'App... works!';
 }
